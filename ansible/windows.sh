42f6a1b94653f60fcb2911c827eab2f1a8f13d0c
Hiroo Kawai <h_kawai@lecre.jp>
2017-01-01 15:29:05 +0900

    cent7用に修正

--- ansible/windows.sh
+++ ansible/windows.sh
@@ -7,9 +7,7 @@ sudo yum -y install yum-plugin-fastestmirror
 sudo yum -y update
 
 # Add Ansible Repository & Install Ansible
-sudo wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
-sudo rpm -Uvh epel-release-6-8.noarch.rpm
-sudo yum -y install --enablerepo=ansible
+sudo yum -y install epel-release
 sudo yum -y install ansible
 
 # Setup Ansible for Local Use and Run
